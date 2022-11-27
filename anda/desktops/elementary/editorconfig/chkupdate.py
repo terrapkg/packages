11e281f300c85b6e2bb56bbfd135f622b206d89c
windowsboy111 <wboy111@outlook.com>
2022-11-27 16:48:54 +0800

    Update chkupdate.py

--- anda/desktops/elementary/editorconfig/chkupdate.py
+++ anda/desktops/elementary/editorconfig/chkupdate.py
@@ -10,6 +10,7 @@ LINK = f'https://api.github.com/repos/{REPO}/releases/latest'
 
 
 ver = requests.get(LINK, headers={'Authorization': 'Bearer ' + os.getenv('GITHUB_TOKEN')}).json()['tag_name']
+ver = ver[1:]
 with open(SPEC, 'r') as f:
     matches = re.findall(REGEX_VER, f.read())
 if not len(matches): exit(f"{NAME}: Failed to match regex!")
