8cd366eab597040c0b710af31409c12e7b3f6f0d
seth <getchoo@tuta.io>
2022-12-05 02:25:07 -0500

    support auto updating more packages

---
+++ anda/moby-extras/moby-buildx/chkupdate.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import re
+
+NAME = "moby-buildx"
+REPO = "docker/buildx"
+REGEX_VER = r"Version:\s*([\.\d]+)\n"
+SPEC = f"{NAME}.spec"
+LINK = f"https://api.github.com/repos/{REPO}/releases/latest"
+
+
+token = os.getenv("GITHUB_TOKEN")
+ver = requests.get(LINK, headers={"Authorization": f"Bearer {token}"}).json()[
+    "tag_name"
+][1:]
+
+with open(SPEC, "r") as f:
+    matches = re.findall(REGEX_VER, f.read())
+
+if not len(matches):
+    exit(f"{NAME}: Failed to match regex!")
+cur = matches[0]
+if ver == cur:
+    exit(f"{NAME}: Up to date!")
+
+print(f"{NAME}: {cur} -> {ver}")
+os.system(f"rpmdev-bumpspec -n {ver} {SPEC}")
