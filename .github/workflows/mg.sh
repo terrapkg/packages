a3c53f35ee68c0b0551c87bebf1cc0fab055610a
windowsboy111 <wboy111@outlook.com>
2023-04-07 20:06:56 +0800

    fix(ci): mg

--- .github/workflows/mg.sh
+++ .github/workflows/mg.sh
@@ -1,12 +1,13 @@
 if $1; then
-	dirs=${$2/\/pkg/}
+	dirs=$2
+	dirs=${dirs/\/pkg/}
 	export p="{\"id\":\"$5\",\"verl\":\"%v\",\"arch\":\"$4\",\"dirs\":\"$dirs\"}"
 else
 	export p="{\"id\":\"$5\",\"verl\":\"%v\",\"arch\":\"$4\"}"
 fi
 for f in anda-build/rpm/rpms/*; do
 	n=$(lesspipe.sh $f | grep -E "Name\s*: " | sed "s@Name\s*: @@")
-	v=$(echo ${f/${n}-/} | sed -E "s@\.fc$3.+@@")
-	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci/terra$3/builds/$n -X PUT -H "Content-Type: application/json" -d ${p/%v/$v} --fail-with-body &
+	v=$(echo ${f/${n}-/} | sed -E "s@\.fc$3.+@@" | sed -E "s@anda-build/rpm/rpms/@@")
+	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci/terra$3/builds/$n -X PUT -H "Content-Type: application/json" -d ${p/\%v/$v} --fail-with-body &
 done
 wait
