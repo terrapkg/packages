0836405a443f66fa69b03b76fb7c8e109bbffe0d
madomado <madonuko@outlook.com>
2024-07-23 22:30:28 +0800

    fix(ci): mg.sh should use new ci5 endpoint (#1607)
    Signed-off-by: madomado <madonuko@outlook.com>

--- .github/workflows/mg.sh
+++ .github/workflows/mg.sh
@@ -17,5 +17,5 @@ for f in anda-build/rpm/rpms/*; do
 	r=$(lesspipe.sh $f | grep -E "Release\s*: " | sed "s@Release\s*: @@")
 	d=${p/\%v/$v}
 	d=${d/\%r/$r}
-	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci/terra$3/builds/$n -X PUT -H "Content-Type: application/json" -d $d --fail-with-body
+	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci5/terra$3/builds/$n -X PUT -H "Content-Type: application/json" -d $d --fail-with-body
 done
