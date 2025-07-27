9c9363a767646c77050eb414797998d83469b974
Gilver <rockgrub@disroot.org>
2025-07-26 22:31:19 -0500

    Revert "chore: Remove zig.squirl.dev until mirror issues are resolved; mirror works again??"
    This reverts commit 19bbd48fd274d8c3f6fd617c6e8f8daada5458e6.

--- anda/langs/zig/choose-mirror.sh
+++ anda/langs/zig/choose-mirror.sh
@@ -4,9 +4,7 @@ version=0.15.0-dev.1232+869ef0060
 
 # Self explanatory
 function randomize_mirrors() {
-  ## Benched mirrors (will be added back if issues with them are resolved):
-  # "https://zig.squirl.dev"
-  mirrors=("https://pkg.machengine.org/zig" "https://zigmirror.hryx.net/zig" "https://zig.linus.dev/zig" "https://zig.florent.dev")
+  mirrors=("https://pkg.machengine.org/zig" "https://zigmirror.hryx.net/zig" "https://zig.linus.dev/zig" "https://zig.squirl.dev" "https://zig.florent.dev")
   number=${#mirrors[@]}
   index=$(( RANDOM % number ))
   mirror=${mirrors[$index]}
@@ -14,7 +12,6 @@ function randomize_mirrors() {
 
 # ONLY export mirrors to the update scripts if they connect on both files
 until curl -If $mirror/zig-${version}.tar.xz &>/dev/null && curl -If $mirror/zig-${version}.tar.xz.minisig &>/dev/null; do
-  echo "404"
   randomize_mirrors
 done
 
