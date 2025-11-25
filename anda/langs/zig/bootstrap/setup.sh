6939c9f36bef3a643b8998ad5730f449bde1f7ca
Gilver <rockgrub@disroot.org>
2025-11-25 04:45:11 -0600

    This is a way better idea

--- anda/langs/zig/bootstrap/setup.sh
+++ anda/langs/zig/bootstrap/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 version=0.16.0-dev.1456+16fc083f2
-mirrorlist="https://pkg.machengine.org/zig https://zigmirror.hryx.net/zig https://zig.linus.dev/zig https://zig.squirl.dev https://zig.florent.dev https://zig.mirror.mschae23.de/zig https://zigmirror.meox.dev https://ziglang.freetls.fastly.net https://zig.tilok.dev https://zig-mirror.tsimnet.eu/zig https://zig.karearl.com/zig https://pkg.earth/zig https://fs.liujiacai.net/zigbuilds"
+
 mirrors=()
 
-for mirror in $mirrorlist; do
+for mirror in $(curl -s https://ziglang.org/download/community-mirrors.txt); do
   mirrors+=($mirror)
 done
 
@@ -17,11 +17,11 @@ function randomize_mirrors() {
 }
 
 if [ "$1" == "fetch" ]; then
-   until curl -If $mirror/zig-${version}.tar.xz &>/dev/null && curl -If $mirror/zig-${version}.tar.xz.minisig &>/dev/null; do
+   until curl -If ${mirror}/zig-${version}.tar.xz &>/dev/null && curl -If ${mirror}/zig-${version}.tar.xz.minisig &>/dev/null; do
      randomize_mirrors
    done
-   curl -A "rpmdev-spectool" -H "Accept-Encoding: identity" -O $mirror/zig-${version}.tar.xz
-   curl -A "rpmdev-spectool" -H "Accept-Encoding: identity" -O $mirror/zig-${version}.tar.xz.minisig
+   curl -A "rpmdev-spectool" -H "Accept-Encoding: identity" -O ${mirror}/zig-${version}.tar.xz
+   curl -A "rpmdev-spectool" -H "Accept-Encoding: identity" -O ${mirror}/zig-${version}.tar.xz.minisig
 elif [ "$1" == "version" ]; then
    echo $version
 # Grab a random mirror. For debugging purposes.
