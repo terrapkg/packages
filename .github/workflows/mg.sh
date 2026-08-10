2c72f934c4a00fa1722c8faa890e27b3acd49b9b
Caleb Jones <caleb@calebgj.io>
2026-08-10 13:24:28 -0500

    fix the fact I accidentally deleted the fail curl

--- .github/workflows/mg.sh
+++ .github/workflows/mg.sh
@@ -10,6 +10,13 @@ fi
 
 export p="{\"id\":\"$5\",\"ver\":\"%v\",\"rel\":\"%r\",\"arch\":\"$4\",\"dirs\":\"$dirs\",\"succ\":$1,\"commit\":\"$7\",\"discord_pings\":\"$pings\"}"
 
+if [[ $1 == false ]]; then
+	d=${p/\%v/?}
+	d=${d/\%r/?}
+	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci5/terra$3/builds/f -X PUT -H "Content-Type: application/json" -d "$d" --fail-with-body
+	exit 0
+fi
+
 for f in anda-build/rpm/rpms/*; do
 	n=$(lesspipe.sh $f | grep -E "Name\s*: " | sed "s@Name\s*: @@" | head -n1)
 	v=$(lesspipe.sh $f | grep -E "Version\s*: " | sed "s@Version\s*: @@" | head -n1)
